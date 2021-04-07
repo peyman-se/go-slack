4bdc26f675d2ddac9cb8f78f4a68867098f47d30
Coder <hello@Hellos-MBP.phub.net.cable.rogers.com>
2021-04-07 11:10:39 -0400

    first commit

---
+++ slack.go
@@ -0,0 +1,54 @@
+package slack
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+type Slack struct {
+	Text string `json:"text"`
+	Attachments []Attachment `json:"attachments"`
+}
+
+type Attachment struct {
+	Title    string   `json:"title"`
+	Text     string   `json:"text"`
+	MarkdownIn []string `json:"mrkdwn_in"`
+	Fields   []Field  `json:"fields"`
+}
+
+type Field struct {
+	Title string `json:"title"`
+	Value int    `json:"value"`
+	Short bool   `json:"short"`
+}
+
+
+func main() {
+	webhook := "https://hooks.slack.com/services/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"
+	Send(webhook, "Test new library")
+}
+
+func Send(webhook string, message string) error {
+	requestBody, _ := json.Marshal(Slack{Text: message})
+	response, err := http.Post(webhook, "application/json", bytes.NewReader(requestBody))
+
+	if err != nil {
+		return err
+	}
+
+	if err != nil {
+		return err
+	}
+
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(response.Body)
+	
+	if buffer.String() != "ok" {
+		return errors.New("Failed to send slack notification")
+	}
+
+	return nil
+}
